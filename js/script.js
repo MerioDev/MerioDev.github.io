7b144a912b81d666369390144f285ba8086c39f3
MerioDev <61060638+MerioDev@users.noreply.github.com>
2020-07-20 13:02:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
 console.log('%cThis website was styled by DerpehDoge. Also, welcome to the dev console.', 'font-size: 2em; color: limegreen; opacity: 70%;');
-console.log('%cGames Made Designed and lisenced by Chris T and or MerioDev. Also, I also welcome you to the dev console.', 'font-size: 2em; color: limegreen; opacity: 70%;');
+console.log('%cGames Made and supplied by Chris T and or MerioDev. Also, I also welcome you to the dev console.', 'font-size: 2em; color: limegreen; opacity: 70%;');
